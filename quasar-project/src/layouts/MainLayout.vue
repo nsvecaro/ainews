5b9db9909b089643718aeb3b36971654667371a0
Manuel-spec-cell <mtauzer@veleri.hr>
2024-11-13 19:30:05 +0100

    Napravljene stranice i linkovi za iste

--- quasar-project/src/layouts/MainLayout.vue
+++ quasar-project/src/layouts/MainLayout.vue
@@ -55,46 +55,24 @@ defineOptions({
 
 const linksList = [
   {
-    title: 'Docs',
-    caption: 'quasar.dev',
-    icon: 'school',
-    link: 'https://quasar.dev'
-  },
-  {
-    title: 'Github',
-    caption: 'github.com/quasarframework',
-    icon: 'code',
-    link: 'https://github.com/quasarframework'
-  },
-  {
-    title: 'Discord Chat Channel',
-    caption: 'chat.quasar.dev',
-    icon: 'chat',
-    link: 'https://chat.quasar.dev'
+    title: 'Home',
+    icon: 'home',
+    link: '#/'
   },
   {
     title: 'Forum',
-    caption: 'forum.quasar.dev',
-    icon: 'record_voice_over',
-    link: 'https://forum.quasar.dev'
+    icon: 'forum',
+    link: '#/forum'
   },
   {
-    title: 'Twitter',
-    caption: '@quasarframework',
-    icon: 'rss_feed',
-    link: 'https://twitter.quasar.dev'
+    title: 'Login',
+    icon: 'login',
+    link: '#/login'
   },
   {
-    title: 'Facebook',
-    caption: '@QuasarFramework',
-    icon: 'public',
-    link: 'https://facebook.quasar.dev'
-  },
-  {
-    title: 'Quasar Awesome',
-    caption: 'Community Quasar projects',
-    icon: 'favorite',
-    link: 'https://awesome.quasar.dev'
+    title: 'Register',
+    icon: 'person_add',
+    link: '#/register'
   }
 ]
 
--- quasar-project/src/router/routes.js
+++ quasar-project/src/router/routes.js
@@ -3,7 +3,10 @@ const routes = [
     path: '/',
     component: () => import('layouts/MainLayout.vue'),
     children: [
-      { path: '', component: () => import('pages/IndexPage.vue') }
+      { path: '#/', component: () => import('pages/IndexPage.vue') },
+      { path: '#/forum', component: () => import('pages/ForumPage.vue') },
+      { path: '#/login', component: () => import('pages/LoginPage.vue') },
+      { path: '#/register', component: () => import('pages/RegisterPage.vue') }
     ]
   },
 
