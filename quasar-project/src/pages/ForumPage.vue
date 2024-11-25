15e3929c06df62fcfa8c5f0bb5d5928fcbf03714
nsvecaro <noelsveca@gmail.com>
2024-11-25 18:14:58 +0100

    Footer na svakoj stranici, maknut q-page 'padding'

--- quasar-project/src/pages/ForumPage.vue
+++ quasar-project/src/pages/ForumPage.vue
@@ -1,6 +1,20 @@
 <template>
-  <q-page padding>
+  <q-page>
     <!-- content -->
+
+<div class="footer">
+
+<div class="footerinfo">
+  <h3>AI NEWS</h3>
+  <p>All rights reserved by AINews &#169;</p>
+</div>
+<div class="socialmedia">
+  <a href="www.instagram.com"><img src="/src/assets/igIcon.png" alt="Instagram" width="20px">Instagram</a>
+  <a href="www.instagram.com"><img src="/src/assets/igIcon.png" alt="Instagram" width="20px">Facebook</a>
+  <a href="www.instagram.com"><img src="/src/assets/igIcon.png" alt="Instagram" width="20px">X</a>
+</div>
+
+</div>
   </q-page>
 </template>
 
@@ -9,3 +23,49 @@ export default {
   // name: 'PageName',
 }
 </script>
+
+<style>
+
+.footer{
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
+  height: 250px;
+  background-color: black;
+  color: white;
+  padding: 0 20px;
+  box-sizing: border-box;
+
+}
+.footerinfo{
+  flex: 1;
+  text-align: left;
+  margin-left: 170px;
+}
+
+.footerinfo h3, .footerinfo p{
+  margin: 5px 0;
+}
+
+.socialmedia{
+  display: flex;
+  flex-direction: column;
+  gap: 5px;
+  align-items: left;
+  margin-right: 170px;
+}
+.socialmedia a{
+  display: flex;
+  align-items: center;
+  text-decoration: none;
+  color: white;
+}
+.socialmedia a:hover {
+  text-decoration: underline;
+}
+.socialmedia a img{
+  margin-right: 10px;
+} 
+
+</style>
--- quasar-project/src/pages/LoginPage.vue
+++ quasar-project/src/pages/LoginPage.vue
@@ -1,5 +1,5 @@
 <template>
-  <q-page class="flex flex-center login-page">
+  <q-page>
     <div style="width: 100%; max-width: 400px; padding: 20px;">
       <div class="text-h5 text-center q-mb-md">Login</div>
       
@@ -36,6 +36,20 @@
         </div>
       </q-form>
     </div>
+
+<div class="footer">
+
+<div class="footerinfo">
+  <h3>AI NEWS</h3>
+  <p>All rights reserved by AINews &#169;</p>
+</div>
+<div class="socialmedia">
+  <a href="www.instagram.com"><img src="/src/assets/igIcon.png" alt="Instagram" width="20px">Instagram</a>
+  <a href="www.instagram.com"><img src="/src/assets/igIcon.png" alt="Instagram" width="20px">Facebook</a>
+  <a href="www.instagram.com"><img src="/src/assets/igIcon.png" alt="Instagram" width="20px">X</a>
+</div>
+
+</div>
   </q-page>
 </template>
 
@@ -64,4 +78,46 @@ function onLogin() {
   background-color: #f5f5f5; 
   padding: 20px;
 }
+
+.footer{
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
+  height: 250px;
+  background-color: black;
+  color: white;
+  padding: 0 20px;
+  box-sizing: border-box;
+
+}
+.footerinfo{
+  flex: 1;
+  text-align: left;
+  margin-left: 170px;
+}
+
+.footerinfo h3, .footerinfo p{
+  margin: 5px 0;
+}
+
+.socialmedia{
+  display: flex;
+  flex-direction: column;
+  gap: 5px;
+  align-items: left;
+  margin-right: 170px;
+}
+.socialmedia a{
+  display: flex;
+  align-items: center;
+  text-decoration: none;
+  color: white;
+}
+.socialmedia a:hover {
+  text-decoration: underline;
+}
+.socialmedia a img{
+  margin-right: 10px;
+} 
 </style>
