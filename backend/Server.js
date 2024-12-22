3ce8911e784a495ca87448672961ee9a56d1a090
The Penetrator <mopacak@veleri.hr>
2024-12-23 00:05:43 +0100

    Test

--- backend/Server.js
+++ backend/Server.js
@@ -34,7 +34,7 @@ require("./routes/vijesti.routes.js")(app);
 require("./routes/korisnik.routes.js")(app);
 require("./routes/komentar.routes.js")(app);
 require("./routes/teme.routes.js")(app);
-
+//mani je lijep
 
 // Port
 const PORT = process.env.PORT || 3000;
